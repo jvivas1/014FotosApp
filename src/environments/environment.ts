e39e83faa0671fe1edb8beb1a85a85cd86ef83f9
Javier Vivas <oscarvijuarezdeveloper@gmail.com>
2018-05-28 14:15:25 -0500

    Subiendo código para subir fotos a firebase y visualizar las fotos subidas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDYBP_R1VGCE4MPUyto1ZfjN2yOqA_pOA0',
+    authDomain: 'fotosfirebase-27ac8.firebaseapp.com',
+    databaseURL: 'https://fotosfirebase-27ac8.firebaseio.com',
+    projectId: 'fotosfirebase-27ac8',
+    storageBucket: 'fotosfirebase-27ac8.appspot.com',
+    messagingSenderId: '234418839253'
+  }
 };
